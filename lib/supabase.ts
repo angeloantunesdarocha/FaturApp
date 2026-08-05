395272e36da23e8335c542cac860722a59082174
Angelo Antunes da Rocha <seu-email@email.com>
2026-08-05 16:21:31 -0300

    Corrige importação do Supabase e adiciona @supabase/ssr

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,11 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
 // Cliente para Client Components
 export function createClientBrowser() {
-  return createBrowserClient(
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  )
 }
 
 // Cliente para Server Components / Server Actions
@@ -14,5 +13,5 @@ export function createClientServer() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  )
 }
