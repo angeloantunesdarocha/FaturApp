2775356fe91290e763e1daa8be176b0c5d5300fe
Ângelo Antunes da Rocha <angeloantunesdarocha@gmail.com>
2026-08-08 16:16:43 -0300

    fix: use PKCE for browser Google OAuth callback

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,7 +4,12 @@ import { createClient } from '@supabase/supabase-js'
 export function createClientBrowser() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        flowType: 'pkce',
+      },
+    }
   )
 }
 
